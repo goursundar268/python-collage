a85ab218b24e7d96b6930fd94957f58fceeba563
goursundar268 <goursundarghosh63@gmail.com>
2024-04-08 14:48:19 +0530

    ok done

---
+++ Q2.7.py
@@ -0,0 +1,29 @@
+x=(1,4,7,2,5,8,3,6,5,4,6,3,9)
+
+#len()
+a=len(x)
+print(f"lenth of tupple is {a}")
+
+#index()
+b=x.index(3)
+print(f"tupple position is {b}")
+
+#min()
+c=min(x)
+print(f"Min of the tupple is {c}")
+
+#max()
+d=max(x)
+print(f"Max of the tupple is {d}")
+
+#sum()
+e=sum(x)
+print(f"tupple sum is {e}")
+
+#sorted()
+f=sorted(x)
+print(f"tupple sorted part is {f}")
+
+#count()
+g=x.count(5)
+print(f"tupple total count is {g}")
